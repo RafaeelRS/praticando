53c5fdd584727326950cfcd8495bf6b84f288778
Rafael <rafars.team22@gmail.com>
2024-09-15 19:36:45 -0300

    Create index.php

---
+++ php/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Praticando PHP</title>
+</head>
+<body>
+    <?php
+    function myMessage() {
+        echo "Hello world!";
+      }
+    ?>
+</body>
+</html>
